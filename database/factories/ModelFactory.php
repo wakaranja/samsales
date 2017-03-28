39559f51a5b5bf026569e1e46b6593c53ebee86b
wakaranja <sirkaranja@gmail.com>
2017-03-28 11:17:07 +0300

    Added Seeds

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,18 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Property::class, function (Faker\Generator $faker) {
+
+
+    return [
+        'name' => $faker->sentences($nb = 1, $asText = true),
+        'client' => $faker->name,
+        'sale_type' => $faker->randomElement($array = array ('cash','hire purchase')),
+        'date_registered' => $faker->dateTimeThisMonth($max = 'now', $timezone = date_default_timezone_get()),
+        'deal_status' => $faker->randomElement($array = array ('pending','closed')),
+        'review' => $faker->sentences($nb = 3, $asText = true),
+        'user_id' => $faker->randomDigitNotNull,
+        'image' => $faker->randomElement($array = array ('property2.jpg','property3.jpg','property4.jpg')),
+    ];
+});
